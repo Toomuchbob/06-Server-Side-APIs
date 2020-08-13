3223674e004e5d86160f7274dbe2d42d717a8929
Gino Zaccardelli <ginozacc@gmail.com>
2020-08-13 17:47:04 -0400

    everything functioning except 5day

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,11 @@
-// GIVEN a weather dashboard with form inputs
-    // WHEN I search for a city
-    // THEN I am presented with current and future conditions for that city and that city is added to the search history
-    // WHEN I view current weather conditions for that city
-    // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-    // WHEN I view the UV index
-    // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-// WHEN I open the weather dashboard
-// THEN I am presented with the last searched city forecast
+
 var lastSearch;
 var prevSearched = [];
 var currentDate = moment().format('MM/DD/YYYY');
 
+var weatherEl = $('#weather-content-id');
 var weatherIconEl  = $("#weather-content-icon-id");
 var weatherCityEl  = $("#weather-content-city-id");
 var weatherTempEl  = $('#weather-content-temp-id');
@@ -30,9 +21,10 @@ $(document).ready(function () {
         $('#input-search-id').val('');
         $('#list-search-id').empty();
 
-        if (localStorage.getItem('prevSearched')) {
+        if (localStorage.getItem('lastSearch')) {
             prevSearched = JSON.parse(localStorage.getItem('prevSearched'));
             lastSearch = localStorage.getItem('lastSearch');
+            getWeather(lastSearch);
         };
 
         $.each(prevSearched, function (index, value) {
@@ -51,6 +43,8 @@ $(document).ready(function () {
             url: queryURL,
             method: 'GET'
         }).then(function (response) {
+
+            weatherEl.removeClass('d-none');
 
             weatherIconEl.attr('src', 'http://openweathermap.org/img/wn/' + response.weather[0].icon + '.png');
             weatherCityEl.text(response.name + " (" + currentDate + ")");
@@ -98,6 +92,8 @@ $(document).ready(function () {
 
     $('#list-search-id').on('click', '.list-group-item', function() {
         getWeather($(this).text());
+        lastSearch = $(this).text();
+        localStorage.setItem('lastSearch', lastSearch);
     });
 
     renderList();
